bc578db36624ca98b3eadee14bf564409417156e
xzy <>
2020-03-31 21:53:25 +0800

    学习Flutter

---
+++ lib/model/post.dart
@@ -0,0 +1,50 @@
+class Post {
+  const Post({
+    this.title,
+    this.author,
+    this.imageUrl,
+  });
+  final String title;
+  final String author;
+  final String imageUrl;
+
+}
+
+final List<Post> posts = [
+  Post(
+      title: 'Candy Shop',
+      author: 'Mohamed Chahin',
+      imageUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1804933958,2478562047&fm=26&gp=0.jpg'
+  ),
+  Post(
+      title: '美妆洗护',
+      author: 'Mohaahin',
+      imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585661088720&di=092ba757938ff07f5ee7058b6c60c78b&imgtype=0&src=http%3A%2F%2Fi1.ileehoo.com%2Fupload%2Fimages%2F2012-05%2F20120528170403991.jpg'
+  ),
+  Post(
+      title: '水密码面膜',
+      author: 'Mohamedhahin',
+      imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585661192300&di=7ec5079572f54f2292b4745df8484ba3&imgtype=0&src=http%3A%2F%2Fimg2.imgtn.bdimg.com%2Fit%2Fu%3D3474178932%2C3744535329%26fm%3D214%26gp%3D0.jpg'
+  ),
+  Post(
+      title: '洗发水',
+      author: 'Mohed Chhin',
+      imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585662492700&di=17c176e811dfb1b6cfdf6cafe2f7483c&imgtype=0&src=http%3A%2F%2Fimg3.imgtn.bdimg.com%2Fit%2Fu%3D337654943%2C3900065108%26fm%3D214%26gp%3D0.jpg'
+  ),  Post(
+      title: 'SK-大写二',
+      author: '顶顶顶顶',
+      imageUrl: 'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1371470252,2366335777&fm=26&gp=0.jpg'
+  ),  Post(
+      title: '水膜',
+      author: 'Mohahin',
+      imageUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1463611678,2001037891&fm=26&gp=0.jpg'
+  ),  Post(
+      title: '清洁补水',
+      author: 'Mohin',
+      imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1585662874623&di=8f8c2b7c33a32518eb524e80d907af8c&imgtype=0&src=http%3A%2F%2Fhbimg.b0.upaiyun.com%2F2b50495af197a98336ce37fdab6c22488a95b546d03f-SouKq8_fw658'
+  ),  Post(
+      title: '西湖龙井',
+      author: '小西湖',
+      imageUrl: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3000960681,536926544&fm=26&gp=0.jpg'
+  ),
+];
