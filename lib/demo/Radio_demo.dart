39e973e648879850af22a81e256fcacf55297d8b
xzy <>
2020-05-13 16:26:01 +0800

    RadioListTitle 单选列表

--- lib/demo/Radio_demo.dart
+++ lib/demo/Radio_demo.dart
@@ -25,21 +25,41 @@ class _RadioDemoState extends State<RadioDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('RadioGroupValue: $_radioGroupA'),
+            SizedBox(height: 32.0),
+            RadioListTile(
+              value: 0,
+              groupValue: _radioGroupA,
+              onChanged: _handleRadioChanged,
+              title: Text('Options A'),
+              subtitle: Text('Description'),
+              secondary: Icon(Icons.filter_1),
+              selected: _radioGroupA == 0,
+            ),
+            RadioListTile(
+              value: 1,
+              groupValue: _radioGroupA,
+              onChanged: _handleRadioChanged,
+              title: Text('Options B'),
+              subtitle: Text('Description'),
+              secondary: Icon(Icons.filter_2),
+              selected: _radioGroupA == 1,
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Radio(//单选
-                  value: 0,
-                  groupValue: _radioGroupA,
-                  onChanged: _handleRadioChanged,
-                  activeColor: Colors.black,
-                ),
-                Radio(//单选
-                  value: 1,
-                  groupValue: _radioGroupA,
-                  onChanged: _handleRadioChanged,
-                  activeColor: Colors.black,
-                ),
+//                Radio(//单选
+//                  value: 0,
+//                  groupValue: _radioGroupA,
+//                  onChanged: _handleRadioChanged,
+//                  activeColor: Colors.black,
+//                ),
+//                Radio(//单选
+//                  value: 1,
+//                  groupValue: _radioGroupA,
+//                  onChanged: _handleRadioChanged,
+//                  activeColor: Colors.black,
+//                ),
               ],
             ),
           ],
