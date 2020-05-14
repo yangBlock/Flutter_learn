5c94fb27cb5452cf8d3c68eb4a3c6ba34e5e95aa
xzy <>
2020-05-14 18:01:21 +0800

    获取及使用选项的值

--- lib/demo/simple_dialog_demo.dart
+++ lib/demo/simple_dialog_demo.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
-
+import 'dart:async';
+enum Option {
+  A,B,C
+}
 class SimpleDialogDemo extends StatefulWidget {
   @override
   _SimpleDialogDemoState createState() => _SimpleDialogDemoState();
 }
 
 class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
-  _openSimpleDialog() {
-    showDialog(
+  String _choice = 'Nothing';
+  Future _openSimpleDialog() async {
+
+    final option =  await showDialog(
       context: context,
       builder: (BuildContext context){
         return SimpleDialog(
@@ -16,25 +21,44 @@ class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
             SimpleDialogOption(
               child: Text('Option A'),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context,Option.A);
               },
             ),
             SimpleDialogOption(
               child: Text('Option B'),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context,Option.B);
               },
             ),
             SimpleDialogOption(
               child: Text('Option C'),
               onPressed: () {
-                Navigator.pop(context);
+                Navigator.pop(context,Option.C);
               },
             ),
           ],
         );
       }
     );
+
+    switch (option) {
+      case Option.A:
+        setState(() {
+          _choice = 'A';
+        });
+        break;
+      case Option.B:
+        setState(() {
+          _choice = 'B';
+        });
+        break;
+      case Option.C:
+        setState(() {
+          _choice = 'C';
+        });
+        break;
+      default:
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -48,6 +72,7 @@ class _SimpleDialogDemoState extends State<SimpleDialogDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Text('Your choice is: $_choice'),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
