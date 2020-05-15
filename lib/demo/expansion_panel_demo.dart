62e48ff6916eca18013960feea1cd4ee139a0bde
xzy <>
2020-05-15 17:43:39 +0800

    ExpansionPanel：面板的收起与展开

--- lib/demo/expansion_panel_demo.dart
+++ lib/demo/expansion_panel_demo.dart
@@ -6,6 +6,8 @@ class ExpansionPanelDemo extends StatefulWidget {
 }
 
 class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
+
+  bool _isExpanded = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +21,12 @@ class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ExpansionPanelList(
+              //点击方法
+              expansionCallback: (int panelIndex, bool isExpanded) {
+                setState(() {
+                  _isExpanded = !isExpanded;
+                });
+              },
               children: [
                 ExpansionPanel(
                   headerBuilder: (BuildContext context, bool isExpanded) {
@@ -35,7 +43,7 @@ class _ExpansionPanelDemoState extends State<ExpansionPanelDemo> {
                     width: double.infinity,
                     child: Text('Content for Panel A.'),
                   ),
-                  isExpanded: false,//是否展开，默认不展开
+                  isExpanded: _isExpanded,//是否展开，默认不展开
                 ),
               ],
             ),
