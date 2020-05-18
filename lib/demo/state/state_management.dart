3100ea17b0a933848c3bef263162085865143ba8
xzy <>
2020-05-18 17:54:08 +0800

    从父辈那里传递个回调

--- lib/demo/state/state_management.dart
+++ lib/demo/state/state_management.dart
@@ -7,6 +7,14 @@ class StateManagementDemo extends StatefulWidget {
 
 class _StateManagementDemoState extends State<StateManagementDemo> {
   int _count = 0;
+
+  //点击回调
+  void _increaseCount () {
+    setState(() {
+      _count += 1;
+    });
+    print(_count);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,15 +22,10 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
         title: Text('StateManagementDemo'),
         elevation: 0.0,
       ),
-      body: Counter(_count),
+      body: Counter(_count,_increaseCount),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {
-          setState(() {
-            _count += 1;
-          });
-          print(_count);
-        },
+        onPressed: _increaseCount,
       ),
     );
   }
@@ -30,13 +33,16 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
 //由父辈管理状态
 class Counter extends StatelessWidget {
   final int  count;
-  Counter(this.count);
+  final VoidCallback increaseCount;
+
+  Counter(this.count,this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Chip(
+      child: ActionChip(
         label: Text('$count'),
+        onPressed: increaseCount,
       ),
     );
   }
