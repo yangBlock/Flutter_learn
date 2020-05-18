cff59f8395f736e5f7f32d0edd35133c65765071
xzy <>
2020-05-18 10:41:28 +0800

    Chip：带删除功能的小碎片

--- lib/demo/chip_demo.dart
+++ lib/demo/chip_demo.dart
@@ -42,6 +42,13 @@ class _ChipDemoState extends State<ChipDemo> {
                     backgroundImage: NetworkImage('http://t7.baidu.com/it/u=2336214222,3541748819&fm=79&app=86&size=h300&n=0&g=4n&f=jpeg?sec=1590142371&t=929b38fea24db098b7326c8eb4856532'),
                   ),
                 ),
+                Chip(
+                  label: Text('City'),
+                  onDeleted: () {},//删除按钮
+                  deleteIcon: Icon(Icons.delete),//删除按钮图标
+                  deleteIconColor: Colors.redAccent,//删除按钮颜色
+                  deleteButtonTooltipMessage: 'Remove this tag',//长按删除按钮提示文字
+                ),
                 Divider(//分隔符
                   color: Colors.grey,
                   height: 32.0,
