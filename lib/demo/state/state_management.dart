544b7d8d41ae76200d2ae5e34c33c7dba508e8c1
xzy <>
2020-05-18 17:48:43 +0800

    状态管理（由父辈管理状态）

--- lib/demo/state/state_management.dart
+++ lib/demo/state/state_management.dart
@@ -14,11 +14,7 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
         title: Text('StateManagementDemo'),
         elevation: 0.0,
       ),
-      body: Center(
-        child: Chip(
-          label: Text('$_count'),
-        ),
-      ),
+      body: Counter(_count),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
@@ -27,6 +23,20 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
           });
           print(_count);
         },
+      ),
+    );
+  }
+}
+//由父辈管理状态
+class Counter extends StatelessWidget {
+  final int  count;
+  Counter(this.count);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Chip(
+        label: Text('$count'),
       ),
     );
   }
