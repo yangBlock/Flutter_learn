fc98f32b6d6b9ec9603cf04ff02c572efdfdad04
xzy <>
2020-05-18 18:21:51 +0800

    创建与使用 InheritedWidget 有效地传递数据给子部件

--- lib/demo/state/state_management.dart
+++ lib/demo/state/state_management.dart
@@ -17,49 +17,62 @@ class _StateManagementDemoState extends State<StateManagementDemo> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('StateManagementDemo'),
-        elevation: 0.0,
-      ),
-      body: CounterWrapper(_count,_increaseCount),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: _increaseCount,
+    return CounterProvider(
+      count: _count,
+      increaseCount: _increaseCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('StateManagementDemo'),
+          elevation: 0.0,
+        ),
+        body: CounterWrapper(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _increaseCount,
+        ),
       ),
     );
   }
 }
 
 class CounterWrapper extends StatelessWidget {
-  final int  count;
-  final VoidCallback increaseCount;
-
-  CounterWrapper(this.count,this.increaseCount);
 
   @override
   Widget build(BuildContext context) {
     return  Center(
-      child: ActionChip(
-        label: Text('$count'),
-        onPressed: increaseCount,
-      ),
+      child: Counter(),
     );
   }
 }
 
 //由父辈管理状态
 class Counter extends StatelessWidget {
-  final int  count;
-  final VoidCallback increaseCount;
-
-  Counter(this.count,this.increaseCount);
-
   @override
   Widget build(BuildContext context) {
+    final int count = CounterProvider.of(context).count;
+    final VoidCallback increaseCount = CounterProvider.of(context).increaseCount;
     return  ActionChip(
       label: Text('$count'),
       onPressed: increaseCount,
     );
+  }
+}
+
+class CounterProvider extends InheritedWidget {
+  final int count;
+  final VoidCallback increaseCount;
+  final Widget child;
+
+  CounterProvider({
+    this.count,
+    this.increaseCount,
+    this.child
+  }) : super(child:child);
+  static CounterProvider of(BuildContext context) => context.dependOnInheritedWidgetOfExactType();
+
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+    return true;
   }
 }
