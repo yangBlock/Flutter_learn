859bbd455199d52c54cf44039a8f839f5f0883fc
xzy <>
2020-05-22 17:50:01 +0800

    使用 Subjects

--- lib/demo/rxdart/rxdart_demo.dart
+++ lib/demo/rxdart/rxdart_demo.dart
@@ -26,11 +26,17 @@ class _RxDartDemoHomeState extends State<RxDartDemoHome> {
     // TODO: implement initState
     super.initState();
 //    Stream<String> _stream =  Stream.fromIterable(['hello','你好']);
-    Stream _stream = Stream.periodic(Duration(seconds: 3),(x) => x.toString());//间隔3秒，打印重复的次数
-    _stream.listen((event) {
-      print(event);
-    });
+//    Stream _stream = Stream.periodic(Duration(seconds: 3),(x) => x.toString());//间隔3秒，打印重复的次数
+//    _stream.listen((event) {
+//      print(event);
+//    });
 
+    PublishSubject<String> _subject = PublishSubject<String>();
+    _subject.listen((value) => print('listen 1: $value'));
+    _subject.add('hello');
+    _subject.listen((value) => print('listen 2: ${value.toUpperCase()}'));
+    _subject.add('hola');
+    _subject.close();
   }
   @override
   Widget build(BuildContext context) {
