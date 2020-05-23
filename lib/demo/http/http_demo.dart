3fc8813941548453682b733e2328e2fd7982de49
xzy <>
2020-05-23 16:52:59 +0800

    6 JSON 转换（dartconvert）

--- lib/demo/http/http_demo.dart
+++ lib/demo/http/http_demo.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -25,7 +27,23 @@ class _HttpDemoHomeState extends State<HttpDemoHome> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    fetchPost();
+//    fetchPost();
+    final post = {
+      'title': 'hello',
+      'description':'nice to meet you.',
+    };
+
+    print(post['title']);
+    print(post['description']);
+
+    final postJson = json.encode(post);
+    print(postJson);
+
+    final postJsonConverted = json.decode(postJson);
+    print(postJsonConverted['title']);
+    print(postJsonConverted['description']);
+    print(postJsonConverted is Map);
+
   }
   fetchPost() async {
 //    https://resources.ninghao.net/demo/posts.json
