7140dffd3d9c0e9047d101a74a60d241206a52d0
xzy <>
2020-05-25 17:50:26 +0800

    8.Integrationtesting：集成测试

---
+++ test/test_driver/app_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_driver/flutter_driver.dart';
+import 'package:flutter_test/flutter_test.dart';
+void main () {
+  group('App', () {
+    FlutterDriver driver;
+
+    setUpAll(() async {
+      driver = await FlutterDriver.connect();
+    });
+
+    tearDownAll(() async {
+      if (driver != null) {
+        driver.close();
+      }
+    });
+  });
+}
