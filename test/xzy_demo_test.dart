5e09110d2f2ab8700839029c55c6d748fbc98bcc
xzy <>
2020-05-25 16:29:34 +0800

    3.Unittesting：单元测试

---
+++ test/xzy_demo_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterapptext/demo/test/test_demo.dart';
+void main () {
+  test('should return hello + something.', () {
+    var string = XZYTestDemo.greet('xzy');
+    expect(string, 'hello xzy');
+  });
+}
