653063c0aef2aed3186ff18b850aa328bf154cac
xzy <>
2020-05-25 17:10:18 +0800

    6.找到并验证小部件（Finder与Matcher的用法）

--- test/xzy_demo_test.dart
+++ test/xzy_demo_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterapptext/demo/test/test_demo.dart';
 void main () {
@@ -6,4 +7,20 @@ void main () {
     var string = XZYTestDemo.greet('xzy');
     expect(string, 'hello xzy~~~');
   });
+
+  //小部件测试
+  testWidgets('widget testing demo.', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: TestDemo()
+      )
+    );
+  });
+
+//有没有指定的文本小部件，显示的文字是hello
+  final labelText = find.text('hello');
+//  expect(labelText, findsNothing);
+  expect(labelText, findsOneWidget);
+//  expect(labelText, findsNWidgets(1));
+
 }
