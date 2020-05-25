bdb75243c899fb0c6afbde16b82ef7233495373f
xzy <>
2020-05-25 13:59:22 +0800

    12 Intl：定义需要翻译的信息

---
+++ lib/demo/i18n/intl/xzy_localizations.dart
@@ -0,0 +1,15 @@
+import 'package:intl/intl.dart';
+class XzyDemoLocalizations {
+  String get title => Intl.message(
+    'hello',
+    name: 'title',
+    desc: 'demo localizations',
+  );
+
+  String greet(String name) => Intl.message(
+    'hello $name',
+    name: 'greet',
+    desc: 'greet someone.',
+    args: [name],
+  );
+}
